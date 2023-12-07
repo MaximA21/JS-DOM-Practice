6cb46c5e3c6bd02d0fafc8f5d6813a0d6bcf4678
Maxim <118841284+MaximA21@users.noreply.github.com>
2023-12-07 20:30:52 +0100

    added tabbed component

--- script.js
+++ script.js
@@ -38,6 +38,21 @@ document.querySelector(".nav__links").addEventListener("click", function (evt) {
         document.querySelector(id).scrollIntoView({behavior:"smooth"})
     }
 })
+//tabbed component
+const tabs = document.querySelectorAll(".operations__tab")
+const tabContainer = document.querySelector(".operations__tab-container")
+const tabsContent = document.querySelectorAll(".operations__content")
+
+tabContainer.addEventListener("click", function (evt) {
+    const clicked = evt.target.closest(".operations__tab")
+    // guard clause
+    if (!clicked) return
+    tabs.forEach(t => t.classList.remove("operations__tab--active"))
+    tabsContent.forEach(c => c.classList.remove("operations__content--active"))
+
+    clicked.classList.add("operations__tab--active")
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add("operations__content--active")
+})
 
 
 
