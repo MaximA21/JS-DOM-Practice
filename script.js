596c2382407b4802e04b5eeeb2fbe52d82c7bce2
Maxim <118841284+MaximA21@users.noreply.github.com>
2023-12-08 16:50:35 +0100

    lazy loading img

--- script.js
+++ script.js
@@ -92,8 +92,20 @@ const revealSection = function (entries, observer) {
     entry.target.classList.remove("section--hidden")
     observer.unobserve(entry.target)
 }
-const sectionObserver = new IntersectionObserver(revealSection, {root: null, threshold: 0.2})
+const sectionObserver =  new IntersectionObserver(revealSection, {root: null, threshold: 0.15})
 allSections.forEach(sec => {
     sectionObserver.observe(sec)
     sec.classList.add("section--hidden")
 })
+
+//lazy loading
+const imgTargets = document.querySelectorAll("img[data-src]")
+const loadImg = function (entries, observer) {
+const [entry] = entries
+    if (!entry.isIntersecting) return
+    entry.target.src = entry.target.dataset.src
+    entry.target.addEventListener("load", () => entry.target.classList.remove("lazy-img"))
+    observer.unobserve(entry.target)
+}
+const imgOberserver = new IntersectionObserver(loadImg, {root: null, threshold: 0, rootMargin: "200px"})
+imgTargets.forEach(img => imgOberserver.observe(img))
